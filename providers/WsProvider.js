236f2903bc53693b24f246d508bc8da17caaf956
Yurii Firs <yurii@wetelo.com>
2017-10-09 14:36:21 +0300

    Support for Adonis v4

--- providers/WsProvider.js
+++ providers/WsProvider.js
@@ -9,20 +9,22 @@
  * file that was distributed with this source code.
 */
 
-const ServiceProvider = require('adonis-fold').ServiceProvider
+const { ServiceProvider } = require('@adonisjs/fold')
+const Session = require('@adonisjs/session/src/Session')
 
 class WsProvider extends ServiceProvider {
-  * register () {
+
+  register () {
     this.app.singleton('Adonis/Addons/Ws', (app) => {
       const Ws = require('../src/Ws')
       const Config = app.use('Adonis/Src/Config')
       const Request = app.use('Adonis/Src/Request')
       const Server = app.use('Adonis/Src/Server')
-      const Session = app.use('Adonis/Src/Session')
       const Helpers = app.use('Adonis/Src/Helpers')
       return new Ws(Config, Request, Server, Session, Helpers)
     })
   }
+
 }
 
 module.exports = WsProvider
